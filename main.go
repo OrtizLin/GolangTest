cc627feef5ad0b5c6153dfb16759bf3169d675d4
OrtizLin <chenghsien852@gmail.com>
2018-03-01 09:58:27 +0800

    hello

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
 }
 
 func homePage_two(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintf(w,"Welcome to the HomePageTwo!")
+	fmt.Fprintf(w, "Welcome to the HomePageTwo!")
 }
 
 func homePage_three(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintf(w,"Welcome to the HomePageThree!")
+	fmt.Fprintf(w, "Welcome to the ee!")
 }
 
 func main() {
-    fmt.Println("listening...")
-         http.HandleFunc("/a", homePage)
-         http.HandleFunc("/b", homePage_two)
-         http.HandleFunc("/c", homePage_three)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-        panic(err)
-    }
+	fmt.Println("listening...")
+	http.HandleFunc("/a", homePage)
+	http.HandleFunc("/b", homePage_two)
+	http.HandleFunc("/c", homePage_three)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
